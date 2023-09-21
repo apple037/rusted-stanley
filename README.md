bcb3dc2992cc5425154afbc28d56c199363e9ea4
jasperfan <jasper@torchtechpro.com>
2023-09-21 15:53:27 +0800

    A rusted chatbot

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rusted-stanley
 A rusted robot needs some oil!🤖
+# Requirements
+```toml
+serenity = "0.11"
+tokio = { version = "1.21.2", features = ["macros", "rt-multi-thread"] }
+serde = "1.0"
+serde_derive = "1.0"
+toml = "0.8.0"
+```
+# TODO
+- [ ] Build a discord bot with rust
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use serde_derive::Deserialize;
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+#[derive(Deserialize)]
+struct Data {
+    keys: Keys,
+}
+
+#[derive(Deserialize)]
+struct Keys {
+    discord: String,
+}
+
+fn read_config() -> Data {
+    let current_dir = std::env::current_dir().expect("current directory");
+    println!("The current directory is {}", current_dir.display());
+    let config_file = std::fs::read_to_string("./src/config.toml").expect("config file");
+    let data: Data = toml::from_str(&config_file).expect("config file");
+    print!("dc: {:?}", data.keys.discord);
+    data
+}
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("#")) // set the bot's prefix to "#"
+        .group(&GENERAL_GROUP);
+    // Get the bot token from the environment
+    let config = read_config();
+
+    // Login with a bot token from the environment
+    let token = config.keys.discord;
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
